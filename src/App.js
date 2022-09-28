7dbcb7cfe81e3b52e15d8ec26dd66c42604d20cb
SaiDeepthiUndavalli <saideepthi976@gmail.com>
2022-09-28 12:49:03 +0000

    password-manager

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import PasswordManager from './Components/PasswordManager'
+import './App.css'
+
+const App = () => (
+  <div>
+    <PasswordManager />
+  </div>
+)
+
+export default App
